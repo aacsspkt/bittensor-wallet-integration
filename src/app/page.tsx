1451438a042ec4e055163422dee0d466e39bc316
Ashish Sapkota <aacsspkt3@gmail.com>
2024-10-08 20:08:23 +0545

    feat: use react hook form for transfer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,49 +1,122 @@
 "use client";
 
+import { useEffect, useState } from "react";
+
+import { useForm } from "react-hook-form";
+
 import { useBittensorAccounts } from "@/hooks/useBittensor";
 import { ApiPromise, WsProvider } from "@polkadot/api";
 import { web3FromAddress } from "@polkadot/extension-dapp";
-import { cryptoWaitReady } from "@polkadot/util-crypto";
+
+interface TransferForm {
+	sender: string;
+	receiver: string;
+	amount: number;
+}
 
 export default function Home() {
 	const { accounts } = useBittensorAccounts();
 	const wsProvider = new WsProvider("wss://entrypoint-finney.opentensor.ai:443");
+	const [response, setResponse] = useState<string | undefined>(undefined);
 
 	console.log("injected accounts", accounts);
 
-	const handleTransferClick = async () => {
+	const { setValue, getValues, handleSubmit } = useForm<TransferForm>({
+		defaultValues: {
+			amount: 0,
+			receiver: "",
+			sender: "",
+		},
+	});
+
+	useEffect(() => {
+		if (accounts && accounts.length) {
+			setValue("sender", accounts[0].address);
+		}
+	}, [accounts, setValue]);
+
+	const handleTransfer = async (data: TransferForm) => {
+		console.log("here");
 		try {
-			await cryptoWaitReady();
+			// await cryptoWaitReady();
 			const api = await ApiPromise.create({ provider: wsProvider });
-			const account = accounts[0];
-			const wallet = await web3FromAddress(account.address);
+
+			const sender = data.sender;
+			const receiver = data.receiver;
+			const amount = data.amount * 1000000000;
+			// const recipient = "5FNaNCPDsJEhTqwY28pVA51LcB3seVY33NRzxUY6myEfim7o";
+
+			const wallet = await web3FromAddress(sender);
 			api.setSigner(wallet.signer);
 
-			const recipient = "5FNaNCPDsJEhTqwY28pVA51LcB3seVY33NRzxUY6myEfim7o";
-			const amount = 1000000000;
+			// console.log("balances:", Object.keys(api.tx.balances));
 
-			console.log("balances:", Object.keys(api.tx.balances));
+			const transfer = api.tx.balances.transferKeepAlive(receiver, amount);
+			const hash = await transfer.signAndSend(sender);
 
-			const transfer = api.tx.balances.transferKeepAlive(recipient, amount);
-			const hash = await transfer.signAndSend(account.address);
-
-			console.log("hash:", hash.toHex());
+			console.log("hash:", hash);
+			console.log("hash human:", hash.toHuman());
+			setResponse(`Success: ${hash.toHuman()}`);
 		} catch (err) {
 			console.error(err);
+			if (err instanceof Error) {
+				setResponse(err.message);
+			}
 		}
 	};
 
 	return (
-		<div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-			<main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
+		<main className="h-screen w-screen flex flex-col justify-center items-center">
+			<form
+				onSubmit={handleSubmit(handleTransfer)}
+				className="w-[800px] grid border dark:border-white p-8 rounded"
+			>
+				<legend className="text-2xl font-bold mb-5">Transfer Tao</legend>
+				<div className="w-full space-y-2 mb-4">
+					<label htmlFor="sender">Sender: </label>
+					<input
+						disabled={true}
+						className="w-full p-2 rounded"
+						placeholder="Sender"
+						type="text"
+						name="sender"
+						id="sender"
+						value={getValues("sender")}
+					/>
+				</div>
+				<div className="w-full space-y-2 mb-4">
+					<label htmlFor="receiver">Receiver: </label>
+					<input
+						className="w-full  p-2 rounded text-black"
+						placeholder="Receiver"
+						type="text"
+						name="receiver"
+						id="receiver"
+						value={getValues("receiver") != "" ? getValues("receiver") : undefined}
+						onChange={(e) => setValue("receiver", e.target.value)}
+					/>
+				</div>
+				<div className="w-full space-y-2 mb-6">
+					<label htmlFor="amount">Amount: </label>
+					<input
+						className="w-full  p-2 rounded text-black"
+						placeholder="Amount"
+						type="number"
+						name="amount"
+						step={"any"}
+						value={getValues("amount") > 0 ? getValues("amount") : undefined}
+						onChange={(e) => setValue("amount", Number(e.target.value))}
+						id="amount"
+					/>
+				</div>
 				<button
-					onClick={handleTransferClick}
-					type="button"
-					className="p-2 h-9 w-24 text-center items-center bg-blue-500 hover:bg-blue-400 text-black hover:text-opacity-80"
+					type="submit"
+					className="p-2 h-9 w-24 mb-6 rounded text-center items-center bg-blue-500 hover:bg-blue-400 text-black hover:text-opacity-80"
 				>
 					Transfer
 				</button>
-			</main>
-		</div>
+				<p className={!response ? "hidden" : ""}>{response}</p>
+			</form>
+		</main>
 	);
 }
